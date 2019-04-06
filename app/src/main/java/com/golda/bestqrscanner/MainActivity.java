0d7ec7ffeefca0fcf5b17dd47ce34ad660712415
zzz <zz88@tut.by>
2019-04-06 13:56:19 +0300

    Add baner

--- app/src/main/java/com/golda/bestqrscanner/MainActivity.java
+++ app/src/main/java/com/golda/bestqrscanner/MainActivity.java
@@ -3,11 +3,23 @@ package com.golda.bestqrscanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-3602084545548553~3024646952");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }

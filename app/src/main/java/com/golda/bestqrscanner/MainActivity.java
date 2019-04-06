4c38755229c863fa44dd231d86f7f9694b603598
user <ijhkljh@jhgfgf.ru>
2019-04-06 13:49:03 +0300

    startActivityForResult

--- app/src/main/java/com/golda/bestqrscanner/MainActivity.java
+++ app/src/main/java/com/golda/bestqrscanner/MainActivity.java
@@ -1,13 +1,57 @@
 package com.golda.bestqrscanner;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button start;
+    private Button link;
+    private int requestCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        start = (Button) findViewById(R.id.start);
+        link = (Button) findViewById(R.id.link);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startScanner();
+            }
+        });
+
+
+
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
+
+
+
+    private void startScanner(){
+        Intent intent = new Intent(this,)
+        startActivityForResult(intent,requestCode);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+
+
 }
